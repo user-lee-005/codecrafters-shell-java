1e2d1953e75733db0cfd4d01241d3eb5ca9999c6
user-lee-005 <93381862+user-lee-005@users.noreply.github.com>
2025-12-02 21:47:34 +0530

    Exit command added

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,16 @@
+import java.util.Objects;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws Exception {
+        while(true) {
          System.out.print("$ ");
          Scanner scanner = new Scanner(System.in);
          String command = scanner.nextLine();
-        System.out.println(command + ": command not found");
+         if(Objects.equals(command, "exit")) {
+             break;
+         }
+         System.out.println(command + ": command not found");
+        }
     }
 }
