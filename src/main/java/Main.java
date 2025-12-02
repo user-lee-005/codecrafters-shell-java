c937eb57de8d0ab5443c0a1f0e39d6762df75c39
user-lee-005 <93381862+user-lee-005@users.noreply.github.com>
2025-12-02 21:42:53 +0530

    Invalid Commands

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,5 +3,8 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) throws Exception {
          System.out.print("$ ");
+         Scanner scanner = new Scanner(System.in);
+         String command = scanner.nextLine();
+        System.out.println(command + ": command not found");
     }
 }
