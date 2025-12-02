43c84197333306bb7873e1f334f42d622d4e97e5
user-lee-005 <93381862+user-lee-005@users.noreply.github.com>
2025-12-02 22:09:15 +0530

    Added Support for type command

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,10 @@
 import utils.StringUtils;
 
-import java.util.Objects;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
+    public static String[] allowedCommands = new String[] {"echo", "exit", "type"};
     public static void main(String[] args) throws Exception {
         while(true) {
              System.out.print("$ ");
@@ -17,10 +18,24 @@ public class Main {
         if(StringUtils.equals(command, "exit")) {
             return true;
         } else if(StringUtils.startsWith(command, "echo")) {
-            System.out.println(command.split("echo")[1].trim());
-        } else {
+            System.out.println(getArgs(command, "echo"));
+        } else if(StringUtils.startsWith(command, "type")) {
+            String arg = getArgs(command, "type");
+            if(Arrays.stream(allowedCommands).anyMatch(cmd -> StringUtils.equals(cmd, arg))) {
+                System.out.println(arg + " is a shell builtin");
+            } else {
+                System.out.println(arg + ": not found");
+            }
+        }
+
+        else {
             System.out.println(command + ": command not found");
         }
         return false;
+    }
+
+    private static String getArgs(String args, String command) {
+        int commandLength = command.length();
+        return args.substring(commandLength + 1).trim();
     }
 }
